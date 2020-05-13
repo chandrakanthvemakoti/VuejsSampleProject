07d03cef44bf20f71547fe73ae908967411ca151
chandrakanthvemakoti <chanduvemakoti@gmail.com>
2020-05-13 17:14:24 +0530

    responsive login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,79 +1,138 @@
 <template>
-  <div class="text-center main">
-  <v-row>
-    <v-container class="con">
-      <div >
-      <v-img class="img" style="text-align:center"  src= "../assets/images/ico.png" alt="icooo">
 
-      </v-img>
-      </div>
-
-      <h3 class="please">Please login to your account</h3>
-      
-  <br>
- <v-form
-   class="form"
-    ref="form"
+  <v-container fluid class="top">
+    
+      <v-row>
+    <v-container class="middle">
+      <v-form 
+       ref="form"
     v-model="valid"
-    lazy-validation
-  >
-    
-  
-          <v-text-field
-          class="user flat"
-            v-model="username"
-            
+    lazy-validation>
+
+      <v-row justify="center">
+      <v-col cols="5" sm="3" md="2"> 
+
+       <v-img class="image ml-5 mt-8" src="../assets/images/ico.png">
+
+       </v-img>
+        
+      </v-col>
+      </v-row>
+      <br>
+      <br>
+       <v-row justify="center">
+      <v-col cols="11" sm="12" md="9"> 
+
+       <h1 class=" mt-12 text-wrap please" >Please login <v-span class="acc">to your account</v-span></h1>
+
+       
+        
+      </v-col>
+      </v-row>
+
+
+
+     <v-row justify="center">
+      <v-col cols="12" sm="6" md="5"> 
+
+       <v-text-field
+       class="user"
+       v-model="username"
       :rules="usernameRules"
-      label="Username"
-      required
-      solo
-          ></v-text-field>
+       label="Username"
+       solo
+       
+      outlined
       
-
- 
+           
+          
+       required>
+            
   
-          <v-text-field
-          class="pass"
-           v-model="password"
-      :rules="passwordRules"
-      label="Password"
-      required
-      solo
-          ></v-text-field>
-      
+       </v-text-field>
+
+       
+        
+      </v-col>
+      </v-row>
+
     
+     <v-row justify="center">
+      <v-col cols="12" sm="6" md="5"> 
 
-    <v-btn
-      :disabled="!valid"
-      
-      class="mr-4 login "
-      @click="validate"
-    >
-     <p class="lognow"> LOGIN NOW</p>
-    </v-btn>
+       <v-text-field
+       class="pass"
+       label="Password"
+       v-model="password"
+        type="password"
+        :rules="passwordRules"
+        required
+        outlined
+        
+        
+        solo>
+            
+  
+       </v-text-field>
 
-    <p class="forgot">Forgot your password?</p>
-  </v-form>
+       
+        
+      </v-col>
+      </v-row>
+        
+
+        <v-row justify="center">
+      <v-col cols="12" sm="6" md="4"> 
+
       
+      <v-btn 
+      class="login " 
+       block 
+       rounded
+       x-large
+       
+      >
+      <v-span class="logtext">LOGIN NOW</v-span>
+        
+      </v-btn>
+
+       
+        
+      </v-col>
+      </v-row>
+
+
+   <v-row justify="center">
+      <v-col cols="12" sm="8" md="5"> 
+
+       <v-text class="forgot ">
+         Forgot your password?
+       </v-text>
+
+       
+        
+      </v-col>
+      </v-row>
+      </v-form>
+
     </v-container>
-    </v-row>
-  </div>
+     
+</v-row>
+  </v-container>
+
 </template>
 
 <script>
-
-
 export default {
-  
+
+
   data(){
     return{
-     items:{
-      src: require("../assets/images/ico.png"),
-      },
+     
    valid: true,
       username: '',
       usernameRules: [
-        v => !!v || 'Userame is required',
+        v => !!v || 'Username is required',
         
       ],
       password: '',
@@ -97,77 +156,62 @@ export default {
     }
   }
 
-
-    
   
 
 </script>
 
 <style scoped>
+
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');
 
-.login{
-  text-align: center !important;
-  margin-top: 20px;
-  
- width: 406px !important;
-  height: 56px !important;
-  object-fit: contain;
-  border-radius: 14px;
-  background-image: linear-gradient(98deg, #fb7f3e, #fa496e);
-  
+.top{
+  background-color: #f4f4f6;
+  width: 100%;
+  height: 900px;
 }
-.lognow{
-  margin-top: 15px;
-  width: 99px !important;
-  height: 26px !important;
-  font-family: Montserrat;
-  font-size: 16px !important;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.63;
-  letter-spacing: normal;
-  text-align: center;
-  color: #ffffff
-}
-
-.user
-{
-
-  margin-top: 30px;
-   margin-left: 280px !important;
-   width: 466px;
-  height: 56px;
-  border-radius: 9px;
-  background-color: #f4f4f6 !important;
-}
-.pass{
-  margin-top: 15px !important;
- margin-left: 280px !important;
-   width: 466px;
-  height: 56px;
-  border-radius: 9px;
-  background-color: #f4f4f6 !important;
-}
-.con{
- margin-top: 60px !important;
-   width: 1060px !important;
-  height: 780px !important;
+.middle{
+   margin-top: 30px;
+  width: 100%;
+  height: 780px;
   border-radius: 10px;
   box-shadow: 3px 10px 26px 0 rgba(0, 0, 0, 0.08);
   background-color: #ffffff;
-  text-align: center !important;
+}
+.image{
+  width: 80px;
+  height: 80px;
+}
+
+.login{
+ 
+  height: 56px !important;
+ 
+  
+  background-image: linear-gradient(98deg, #fb7f3e, #fa496e);
+}
+
+
+.user{
+  width: 466px !important;
+  height: 56px !important;
+  border-radius: 9px;
   
 }
 
+
+.pass{
+  width: 466px !important;
+  height: 56px !important;
+  border-radius: 9px;
+
+}
+
 .forgot{
-  margin-top: 30px;
-  margin-left: 420px;
+
   width: 178px !important;
   height: 22px !important;
-  
-  font-size: 16px !important;
+  margin-left: 140px;
+  font-size: 18px !important;
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
@@ -177,119 +221,196 @@ export default {
   color: #0095c4;
 }
 
-.main{
-   width: 1440px !important;
-  height: 900px !important;
-  background-color: #f4f4f6;
-}
+.logtext{
 
-.please{
- margin-left: 320px;
-   margin-top: 90px !important;
-    width: 400px ;
-  height: 34px;
-  font-family: Montserrat !important;
-  font-size: 26px !important;
+  width: 99px;
+  height: 26px;
+  font-family: Montserrat;
+  font-size: 16px;
   font-weight: bold;
- 
-  line-height: 1.31;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.63;
   letter-spacing: normal;
- 
-  color: var(--dark);
-}
-.img{ 
-  text-align: center !important;
-  margin-top: 50px;
- margin-left: 450px;
-   width: 78px;
-  height: 79px;
-}
- 
+  text-align: center;
+  color: #ffffff;
 
- @media screen and (max-width:1200px ){
-   .main{
+}
+ .please{
+ margin-left: 230px;
+  display: inline;
+ 
+  font-family: Montserrat;
+  font-size: 26px;
+  font-weight: bold;
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@media screen  and (max-width:800px ){
+  .middle{
+    margin: 0%;
+    height: 100% !important;
+  }
+  
+}
+
+@media screen  and (max-width:1260px ){
+  .please{
+    
+    margin-left: 130px;
+  }
+  
+}
+
+@media screen  and (max-width:960px ){
+  .please{
+    
+    margin-left: 250px;
+  }
+  
+}
+ @media screen  and (max-width:860px ){
+  .please{
      width: 100%;
-   }
- }
 
- @media screen and (max-width:1200px ){
-   .please{
-     width: 50%;
-     margin-left: 200px;
-   }
- }
- @media screen and (max-width:1200px ){
-   .user{
-     width: 50%;
-     margin-left: 200px !important;
-   }
- }
-  @media screen and (max-width:1200px ){
-   .pass{
-     width: 50%;
-     margin-left: 200px !important;
-   }
- }
- @media screen and (max-width:1200px ){
-   .login{
-     width: 50%;
-     margin-left: -25px !important;
-   }
- }
-  @media screen and (max-width:1200px ){
-   .forgot{
-     width: 50%;
-     margin-left: 340px !important;
-   }
- }
-  @media screen and (max-width:1200px ){
-   .img{
-     width: 10%;
-     margin-left: 370px !important;
-   }
- }
-  @media screen and (max-width:960px ){
-   .con{
-     width: 50%;
-    
-   }
- }
- @media screen and (max-width:960px ){
-   .please{
-     width: 50%;
-     margin-left: 220px !important;
-    
-   }
- }
-  @media screen and (max-width:960px ){
-   .user{
-     width: 50%;
-     margin-left: 240px !important;
-    
-   }
- }
+    margin-left: 200px;
+  }
+  
+}
+@media screen  and (max-width:760px ){
+  .please{
+   margin-left: 150px;
+  }
+  
+}
 
-  @media screen and (max-width:960px ){
-   .pass{
-     width: 50%;
-     margin-left: 240px !important;
+@media screen  and (max-width:660px ){
+  .please{
+   margin-left: 110px;
+   
+  }
+  
+}
+
+
+@media screen  and (max-width:600px ){
+  .please{
+   margin-left: 20px;
+  }
+  
+}
+
+ @media screen  and (max-width:600px ){
+  .user{
     
-   }
- }
+    width: 100% !important;
+
+  }
+  
+}
+@media screen  and (max-width:600px ){
+  .forgot{
+    margin-left: 120px;
+    width: 100% !important;
+  }
+  
+}
+
+
+ @media screen  and (max-width:600px ){
+  .pass{
+    
+    width: 100% !important;
+  }
+  
+}
+
+@media screen  and (max-width:600px ){
+  .forgot{
+    margin-left: 180px !important;
+    width: 100% !important;
+  }
+  
+}
+@media screen  and (max-width:1260px ){
+  .forgot{
+    margin-left: 80px;
+    width: 100% !important;
+  }
+  
+}
+@media screen  and (max-width:960px ){
+  .forgot{
+    margin-left: 220px;
+    width: 100% !important;
+  }
+  
+}
+
+@media screen  and (max-width:860px ){
+  .forgot{
+    margin-left: 160px;
+    width: 100% !important;
+  }
+  
+}
+
+@media screen  and (max-width:760px ){
+  .forgot{
+    margin-left: 120px;
+    width: 100% !important;
+  }
+  
+}
+@media screen  and (max-width:660px ){
+  .forgot{
+    margin-left: 120px;
+    width: 100% !important;
+  }
+  
+}
+
+@media screen  and (max-width:560px ){
+  .forgot{
+    margin-left: 140px !important;
+    width: 100% !important;
+  }
+  
+}
+@media screen  and (max-width:460px ){
+  .forgot{
+    margin-left: 100px !important;
+    width: 100% !important;
+  }
+  
+}
+
+@media screen  and (max-width:360px ){
+  .forgot{
+    margin-left: 70px !important;
+    width: 100% !important;
+  }
+  
+}
  
-  @media screen and (max-width:960px ){
-   .forgot{
-     width: 50%;
-     margin-left: 420px !important;
-    
-   }
- }
 
-  @media screen and (max-width:960px ){
-   .img{
-     width: 10%;
-     margin-left: 420px !important;
-    
-   }
- }
+
 </style>
